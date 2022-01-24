a0afe80c7f7de0ff99755f05e18600a5906d07c1
Nikolay <94299978+vinel-valet@users.noreply.github.com>
2022-01-24 17:30:08 +0300

    training redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import { createStore} from "redux";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reduser from "./reducer";
+import {Provider} from "react-redux";
+import App from "./components/app";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(reduser);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
